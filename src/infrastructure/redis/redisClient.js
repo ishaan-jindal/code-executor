d9d5aebb8d3ecc17c13bdc06935284644e21d71c
Ishaan Jindal <ishaanjindal2006@gmail.com>
2026-04-17 22:30:48 +0530

    use REDIS_URL from .env

--- src/infrastructure/redis/redisClient.js
+++ src/infrastructure/redis/redisClient.js
@@ -1,14 +1,12 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
+const redisUrl = process.env.REDIS_URL;
+
+export const redis = new Redis(redisUrl, {
   retryStrategy: (times) => Math.min(times * 50, 2000),
 });
 
-export const redisBlocking = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
+export const redisBlocking = new Redis(redisUrl, {
   retryStrategy: (times) => Math.min(times * 50, 2000),
 });
 
